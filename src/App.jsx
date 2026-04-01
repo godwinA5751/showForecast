e509d81def9d2d0d7be9970dc37b27ae180a082e
Godwin <godwinaugustine118@gmail.com>
2026-04-01 20:37:46 +0100

    minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router'
+import { Routes, Route } from 'react-router-dom'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { CityWeather } from './components/CityWeather'
